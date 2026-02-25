4fb6e1c32c785fda0e086d2142b915ffcab4fe8b
Kaushlendra <kaushlendrasingh3841@gmail.com>
2026-02-25 09:49:08 +0530

    Switch to PyMySQL for Railway compatibility

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
+import pymysql
+pymysql.install_as_MySQLdb()
 from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
